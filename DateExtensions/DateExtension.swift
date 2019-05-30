f359d8af6c28aaec5a27a4e5f9dc6839dc25fcfd
Naresh Nallamsetty <naresh.n@imimobile.com>
2019-05-30 13:28:26 +0530

    Initial Commit

---
+++ DateExtensions/DateExtension.swift
@@ -0,0 +1,18 @@
+//
+//  DateExtension.swift
+//  DateExtensions
+//
+//  Created by Naresh Nallamsetty on 30/05/19.
+//  Copyright © 2019 IMIMobile. All rights reserved.
+//
+
+import Foundation
+class DateExtension {
+    
+}
+
+extension Date {
+    func getTheTimeFormattedStringFor(date: Date) -> String {
+        return "Time formatted string"
+    }
+}
